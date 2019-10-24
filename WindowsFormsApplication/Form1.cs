b46a44062b445023b8febbdeecabb690e666f242
Panagiotis Prattis <panagiotispr1997@gmail.com>
2019-10-24 12:50:45 +0300

    Add files via upload

---
+++ WindowsFormsApplication/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+namespace WindowsFormsApplication5._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            textBox2.PasswordChar = '*';
+            textBox3.PasswordChar = '*';
+
+            label3.Visible = false;//error message
+            label3.Text = "error message";
+
+            button2.Text = "register";
+            textBox3.Visible = false;//repeat password
+            label5.Visible = false;
+            label4.Text = "Don't have an account?";
+            label6.Visible = false;
+            textBox4.Visible = false;//name textbox
+
+        }
+
+        public class Global//shared values between Form1 and Form2
+        {
+            public static string username;
+            public static int id;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int repeats = 0;//to find the place of the user in the table
+            int showerr = 0;
+            errtimer.Stop();
+            Global.username = "";
+            const string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=userdatabase.mdb; Persist Security Info=False;";
+
+            if (button1.Text == "login")//the login menu
+            {
+                if (textBox2.Text == "" || textBox1.Text == "")
+                {
+                    errtimercounter = 0;
+                    errtimer.Start();
+                }
+                else
+                {
+                    try//will try to find the user
+                    {
+                        OleDbConnection conn = new OleDbConnection(connectionString);
+                        conn.Open();
+                        string query = "SELECT * FROM table1";
+                        OleDbCommand cmd = new OleDbCommand(query, conn);
+                        OleDbDataReader rdr = cmd.ExecuteReader();
+                        rdr.Read();
+                        StringBuilder builder = new StringBuilder();
+                        while (rdr.Read())
+                        {
+                            repeats++;                
+                            if (rdr.GetString(1) == textBox1.Text && rdr.GetString(2) == textBox2.Text)
+                            {
+                                Global.username = rdr.GetString(3);
+                                Global.id = repeats;
+                                MessageBox.Show("ID " + repeats + ". Welcome user " + Global.username);
+                                errtimer.Stop();
+                            }
+                            else if (rdr.GetString(1) != textBox1.Text)
+                                showerr++;
+                            else if (rdr.GetString(1) == textBox1.Text && rdr.GetString(2) != textBox2.Text)
+                            {
+                                MessageBox.Show("Wrong password");
+                                errtimer.Stop();
+                            }
+                        }
+                        if (repeats == showerr)
+                            MessageBox.Show("Not such user in our database");
+
+                        errtimer.Stop();
+                        
+                        conn.Close();                      
+                    }
+                    catch (Exception)
+                    {
+                        errtimercounter = 0;
+                        if(Global.username == "")
+                            errtimer.Start();
+                    }
+
+                    if (Global.username != "")
+                    {
+                        Form2 f2 = new Form2();
+                        f2.Show();
+                        this.Hide();
+                    }
+                }
+            }
+            else if (button1.Text == "register")//the register menu
+            {
+                samename = false;//to see if there's already such user
+                try
+                {
+                    OleDbConnection conn1 = new OleDbConnection(connectionString);
+                    conn1.Open();
+                    string query1 = "SELECT * FROM table1";
+                    OleDbCommand cmd1 = new OleDbCommand(query1, conn1);
+                    OleDbDataReader rdr = cmd1.ExecuteReader();
+                    rdr.Read();
+                    StringBuilder builder = new StringBuilder();
+                    while (rdr.Read())
+                    {
+                        if (rdr.GetString(1) == textBox1.Text)
+                        {
+                            samename = true;
+                        }
+                    }
+                    conn1.Close();
+                }
+                catch(Exception)
+                {
+                    errtimercounter = 0;
+                    errtimer.Start();
+                }
+
+                if (samename == true)
+                {
+                    textBox1.Clear();
+                    textBox2.Clear();
+                    textBox3.Clear();
+                    textBox4.Clear();                   
+                    errtimercounter = 0;
+                    errtimer.Start();
+
+                }
+                else if (textBox2.Text == "" || textBox3.Text == "" || (textBox2.Text == "" && textBox3.Text == "") || textBox2.Text != textBox3.Text || textBox1.Text == "")
+                {
+                    errtimercounter = 0;
+                    errtimer.Start();
+                }
+                
+                else
+                {
+                    try//try to create user
+                    {
+                        OleDbConnection conn = new OleDbConnection(connectionString);
+                        conn.Open();
+
+                        string uname, upass;
+                        uname = textBox1.Text;
+                        upass = textBox2.Text;
+
+                        string query = ("Insert into table1 (uname, upass, name) values ('"+textBox1.Text+"', '"+textBox2.Text+ "', '"+textBox4.Text+"')");
+                        OleDbCommand cmd = new OleDbCommand(query, conn);
+                        int i = cmd.ExecuteNonQuery();
+
+                        MessageBox.Show("User created");
+                        conn.Close();
+                        errtimer.Stop();
+                        button2.PerformClick();
+                        textBox1.Text = uname;
+                    }
+                    catch (Exception)
+                    {
+                        errtimercounter = 0;
+                        errtimer.Start();
+                    }
+                }
+
+            }
+        }
+        bool samename = false;
+        public int errtimercounter;
+
+        private void button2_Click(object sender, EventArgs e)//change menus
+        {
+            if (button2.Text == "register")
+            {
+                errtimer.Stop();
+                label3.Visible = false;
+                label4.Text = "Already have an account?";
+                button2.Text = "login";
+                button1.Text = "register";
+                textBox3.Visible = true;
+                label5.Visible = true;
+                label6.Visible = true;
+                textBox4.Visible = true;
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+            else
+            {
+                errtimer.Stop();
+                label3.Visible = false;
+                label4.Text = "Don't have an account?";
+                button2.Text = "register";
+                button1.Text = "login";
+                textBox3.Visible = false;
+                label5.Visible = false;
+                label6.Visible = false;
+                textBox4.Visible = false;
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+            }
+        }
+
+        private void errtimer_Tick(object sender, EventArgs e)//the timer for the error messages
+        {
+            if (button1.Text == "login")
+            {
+                if (textBox1.Text == "")
+                {
+                    label3.Visible = true;
+                    label3.Text = "Enter a user name";
+                    errtimercounter++;
+                }
+                else if (textBox2.Text == "")
+                {
+                    label3.Visible = true;
+                    label3.Text = "Enter a password";
+                    errtimercounter++;
+                }
+                else if(Global.username == "")
+                {
+                    label3.Visible = true;
+                    label3.Text = "No connection with the database";
+                    errtimercounter++;
+                }
+
+                if (errtimercounter == 2)
+                {
+                    label3.Visible = false;
+                    label3.Text = "error message";
+                    errtimer.Stop();
+                }
+            }
+            else if (button1.Text == "register")
+            {
+                if (samename == true)
+                {
+                    label3.Visible = true;
+                    label3.Text = "Name already exists";
+                    errtimercounter++;
+                }
+                else
+                {
+                    if (textBox1.Text == "")
+                    {
+                        label3.Visible = true;
+                        label3.Text = "Enter a user name";
+                        errtimercounter++;
+                    }
+                    else if (textBox2.Text == "" || textBox3.Text == "" || (textBox2.Text == "" && textBox3.Text == ""))
+                    {
+                        label3.Visible = true;
+                        label3.Text = "Enter a password";
+                        errtimercounter++;
+                    }
+                    else if (textBox2.Text != textBox3.Text)
+                    {
+                        label3.Visible = true;
+                        label3.Text = "Invalid passwords";
+                        errtimercounter++;
+                    }
+                    else
+                    {
+                        label3.Visible = true;
+                        label3.Text = "No connection with the database";
+                        errtimercounter++;
+                    }
+
+                    if (errtimercounter == 2)
+                    {
+                        label3.Visible = false;
+                        label3.Text = "error message";
+                        errtimer.Stop();
+                    }
+                }
+
+                if (errtimercounter == 2)
+                {
+                    label3.Visible = false;
+                    label3.Text = "error message";
+                    errtimer.Stop();
+                }
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+    
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ WindowsFormsApplication/Form2.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Text;
+
+
+
+namespace WindowsFormsApplication5._1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.Size = new Size(430, 270);//the minimum size
+            textBox1.Enabled = false;
+        }
+
+        public string name;
+        public int id;
+
+        public void Form2_Load(object sender, EventArgs e)//load form
+        {
+            name = Form1.Global.username;//the shared variables
+            id = Form1.Global.id;
+            textBox1.Text ="User " + id +": "+ name;
+            const string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=userdatabase.mdb; Persist Security Info=False;";
+
+            try//try each time to initialize the window with the user's inputs and choices
+            {//the path for the profile image
+                OleDbConnection conn = new OleDbConnection(connectionString);
+                conn.Open();
+                string query = "SELECT * FROM table1";
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                OleDbDataReader rdr = cmd.ExecuteReader();
+                rdr.Read();
+                StringBuilder builder = new StringBuilder();
+
+                while (rdr.Read())
+                {
+                    if(rdr.GetString(3) == name)
+                        pictureBox1.ImageLocation = @rdr.GetString(7);
+                }
+            }
+            catch (Exception) { }//if there's no input it will not load the choice
+
+            try
+            {//the background color
+                OleDbConnection conn = new OleDbConnection(connectionString);
+                conn.Open();
+                string query = "SELECT * FROM table1";
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                OleDbDataReader rdr = cmd.ExecuteReader();
+                rdr.Read();
+                StringBuilder builder = new StringBuilder();
+
+                while (rdr.Read())
+                {
+                    if (rdr.GetString(3) == name)
+                    {
+                        this.BackColor = Color.FromName(rdr.GetString(6));
+                    }
+                        
+                }
+            }
+            catch (Exception) { }
+
+            try
+            {//the dimensions of the window
+                OleDbConnection conn = new OleDbConnection(connectionString);
+                conn.Open();
+                string query = "SELECT * FROM table1";
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                OleDbDataReader rdr = cmd.ExecuteReader();
+                rdr.Read();
+                StringBuilder builder = new StringBuilder();
+
+                while (rdr.Read())
+                {
+                    if (rdr.GetString(3) == name)
+                        this.Size = new Size(Int32.Parse(rdr.GetString(4)), Int32.Parse(rdr.GetString(5)));
+                }
+            }
+            catch (Exception) { }
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)//logout button
+        {
+            Form1 f1 = new Form1();
+            f1.Show();
+            this.Hide();
+            this.BackColor = SystemColors.Control;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)//delete user button
+        {
+
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to delete your profile?", "Delete", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                string connectionstring = "Provider=Microsoft.Jet.OleDb.4.0; Data Source=userdatabase.mdb";
+                OleDbConnection conn = new OleDbConnection(connectionstring);
+                conn.Open();
+                string query = "Delete from table1 where name='" + name + "'";
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                int i = cmd.ExecuteNonQuery();
+                conn.Close();
+                Form1 f1 = new Form1();
+                f1.Show();
+                this.Hide();
+            }
+            
+        }
+
+
+        public Color colorn;
+
+        private void button7_Click(object sender, EventArgs e)//color button
+        {
+            string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=userdatabase.mdb; Persist Security Info=False;";
+
+            ColorDialog MyDialog = new ColorDialog();//create a color dialog
+            MyDialog.SolidColorOnly = true;
+            MyDialog.AllowFullOpen = true;
+            MyDialog.ShowHelp = true;
+
+            if (MyDialog.ShowDialog() == DialogResult.OK)//choosing a non known color will not generate it as backcolor
+            {
+                BackColor = MyDialog.Color;
+                colorn = MyDialog.Color;
+                MessageBox.Show(MyDialog.Color + colorn.ToString());
+                OleDbConnection conn = new OleDbConnection(connectionString);
+                conn.Open();
+
+                string query = "Update table1 set color='" + colorn.ToKnownColor() + "' where name='" + name + "'";
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                int i = cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+
+        public string path;
+
+        private void button6_Click(object sender, EventArgs e)//image button
+        {
+            string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=userdatabase.mdb; Persist Security Info=False;";
+
+            OpenFileDialog of = new OpenFileDialog();
+            of.Filter = "Image Files (*.bmp;*.jpg;*.jpeg,*.png)|*.BMP;*.JPG;*.JPEG;*.PNG";
+            if (of.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.ImageLocation = of.FileName;
+                path = pictureBox1.ImageLocation;
+
+                OleDbConnection conn = new OleDbConnection(connectionString);
+                conn.Open();
+
+                string query = "Update table1 set path='" +path + "' where name='" +name+ "'";
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+                int i = cmd.ExecuteNonQuery();
+                conn.Close();
+            };
+        }
+
+        int parse = 1;
+        private void button3_Click(object sender, EventArgs e)//dimensions button
+        {
+            const string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=userdatabase.mdb; Persist Security Info=False;";
+
+            if (textBox2.Text != null && textBox2.Text != null)//input something
+            {
+                if (Int32.TryParse(textBox2.Text, out parse) == true && Int32.TryParse(textBox3.Text, out parse) == true)//it must be an integer
+                {
+                    if (Int64.Parse(textBox2.Text) >= 430 && Int64.Parse(textBox3.Text) >= 270)//it can't be smaller than the minimum window size
+                    {
+                        OleDbConnection conn = new OleDbConnection(connectionString);
+                        conn.Open();
+
+                        string query = "Update table1 set X='" + textBox2.Text + "' where name='" + name + "'";
+                        OleDbCommand cmd = new OleDbCommand(query, conn);
+                        int i = cmd.ExecuteNonQuery();
+                        string query1 = "Update table1 set Y='" + textBox3.Text + "' where name='" + name + "'";
+                        OleDbCommand cmd1 = new OleDbCommand(query1, conn);
+                        int j = cmd1.ExecuteNonQuery();
+                        conn.Close();
+
+                        this.Size = new Size((Int32.Parse(textBox2.Text)), (Int32.Parse(textBox3.Text)));
+                    }
+                    else
+                    {
+                        MessageBox.Show("Minimum width 430 and minimum heigth 270!");
+                        textBox2.Clear();
+                        textBox3.Clear();
+                    }
+                }
+                else
+                { 
+                    MessageBox.Show("Only integers!");
+                    textBox2.Clear();
+                    textBox3.Clear();
+                }
+            }
+            else
+                MessageBox.Show("Input the dimensions!");
+        }
+    }
+}
